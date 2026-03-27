48a49481b86939e909646b6fe2d7d17ca9829538
Oscar-Fleming <of10@kent.ac.uk>
2026-03-27 01:12:25 +0000

    creation of Content class and basic skeleton code

--- Main.py
+++ Main.py
@@ -6,17 +6,29 @@ class Product:
         self.type = type
 
 def main():
+    logged_in = False
+    print("Welcome to Blockbuster!")
+    while logged_in == False:
+        login_option = int(input("""""would you like to:
+1 - Login
+2 - create account?"""))
+        if login_option == 1:
+            username = input("what is your user name")
+            functionLibrary.
+
+    #loops until the exit choice is made
     while True:
-        print("""Welcome to Blockbuster!
-              Please pick an option:
-              1 - borrow a product
-              2 - return a product
-              3 - recieve a recommendation
-              4 - make a donation
-              5 - exit""")
-        break
+        choice = int(input("""Please pick an option:
+1 - borrow a product
+2 - return a product
+3 - recieve a recommendation
+4 - make a donation
+5 - exit
+"""))
+        if choice == 5:
+            print("Thank you for visiting Blockbuster")
+            break
     return
-    
 # makes sure the code doesnt run if it is called to a seperate file
 if __name__ == "__main__":
     main()
---
+++ functionLibrary.py
@@ -0,0 +1,14 @@
+def usernameChecker(x):
+    try:
+        with open()
+
+
+
+class Content:
+    def __init__(self, media_type, author):
+        if not author:
+            raise ValueError ("please add an author next time")
+        if media_type not in ["book", "dvd", "cd"]
+            raise ValueError ("please include an accepted media type(DVD, CD, Book)")
+        self.media_type = media_type
+        self.author = author
